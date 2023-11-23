98a3c269a37623d6157f25322f892932a3247861
chameronze <penhchamreounpen@gmail.com>
2023-11-23 09:15:51 +0700

    first commit

--- recursion.java
+++ recursion.java
@@ -1,14 +1,22 @@
+import java.util.Scanner;
+
 public class recursion {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter and indes for a Fibonnacci Number: ");
+    int index = input.nextInt();
 
-    //call your recursion fuction
-    //print result
-
+    System.out.println("The Fibonacci number at index " + index + " is " + recursionFunction(index));
+    
    }
     
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+    public static int recursionFunction(int index) {
+      if (index == 0)
+        return 0;
+      else if (index == 1)
+        return 1;
+      else
+        return recursionFunction(index - 1) + recursionFunction(index - 2);
     }
   
 }
